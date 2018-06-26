e3622d46b7748c61215cf13946cddc0020c99ba8
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2018-06-26 14:27:49 -0400

    Update features.vue

--- src/components/features.vue
+++ src/components/features.vue
@@ -5,6 +5,9 @@
     <ul>
       <li v-for="(feature, index) in features" :key="index">
         <a href="#" @click.prevent="openInDefaultBrowser(feature.url)">{{ feature.name }}</a>
+        <small v-if="feature.version">
+          (v{{ feature.version }})
+        </small>
       </li>
     </ul>
   </section>
@@ -17,27 +20,33 @@ module.exports = {
     return {
       features: [
         {
-          name: 'NW.js (v' + nw.process.versions.nw + ')',
+          name: 'NW.js',
+          version: nw.process.versions.nw,
           url: 'https://nwjs.io'
         },
         {
           name: 'NW.js Builder (Phoenix)',
+          version: JSON.parse(nw.require('fs').readFileSync('./node_modules/nwjs-builder-phoenix/package.json').toString()).version,
           url: 'https://github.com/evshiron/nwjs-builder-phoenix'
         },
         {
           name: 'Vue.js',
+          version: Vue.version,
           url: 'https://vuejs.org'
         },
         {
           name: 'Vue-Router',
+          version: VueRouter.version,
           url: 'https://router.vuejs.org/en/'
         },
         {
           name: 'Vuex',
+          version: Vuex.version,
           url: 'https://vuex.vuejs.org/en'
         },
         {
           name: 'HTTP-Vue-Loader',
+          version: JSON.parse(nw.require('fs').readFileSync('./node_modules/http-vue-loader/package.json').toString()).version,
           url: 'https://github.com/FranckFreiburger/http-vue-loader'
         }
       ]
